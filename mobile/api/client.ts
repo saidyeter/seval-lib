63e534e396761dd39d4272e6cdd386702bbfcaef
Said Yeter <kordiseps@gmail.com>
2023-03-14 22:58:37 +0300

    Add default page size to api call #12

--- mobile/api/client.ts
+++ mobile/api/client.ts
@@ -8,34 +8,36 @@ export default {
 }
 
 
-const baseUrl ='http://localhost:3000/api'
+const baseUrl = 'http://localhost:3000/api'
 
 
 
-const paginationSchema= z.object({
-    pageNumber:z.number().optional(),
+const paginationSchema = z.object({
+    pageNumber: z.number().optional(),
     pageSize: z.number().optional(),
 })
 
 const booksParams = paginationSchema.merge(bookSchema)
 
-const booksResultSchema  = z.object({
+const booksResultSchema = z.object({
     totalCount: z.number(),
     pageCount: z.number(),
     items: bookSchema.array()
 })
 
-async function books(params: z.infer<typeof booksParams>):Promise< z.infer<typeof booksResultSchema> | undefined >{
-    
-    const query= Object.entries(params).map(([key, val]) => `${key}=${encodeURIComponent(val)}`).join('&');
-    const url =baseUrl+ '/books/?' + query
+async function books(params: z.infer<typeof booksParams>): Promise<z.infer<typeof booksResultSchema> | undefined> {
+    if (!params.pageSize) {
+        params.pageSize = 10
+    }
+    const query = Object.entries(params).map(([key, val]) => `${key}=${encodeURIComponent(val)}`).join('&');
+    const url = baseUrl + '/books/?' + query
     const res = await fetch(url)
     const resJson = await res.json()
     const validation = booksResultSchema.safeParse(resJson)
     if (validation.success) {
         return validation.data
     }
-    console.log('books validation error',validation.error);
+    console.log('books validation error', validation.error);
     return undefined
-    
+
 }
